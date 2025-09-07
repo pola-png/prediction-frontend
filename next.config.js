a24db884ed5ca65bc8eee04c832abec529919113
Peter Olamilekan <polamedian@gmail.com>
2025-09-07 16:22:35 +0000

    All the cron job keep showing error 500, app is building as a static Nex

---
+++ next.config.js
@@ -0,0 +1,35 @@
+
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+};
+
+module.exports = nextConfig;
