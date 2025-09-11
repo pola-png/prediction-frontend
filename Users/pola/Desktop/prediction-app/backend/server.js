2379176bda1d1933eefaeefb9b79177d2b11e494
Peter Olamilekan <polamedian@gmail.com>
2025-09-11 18:30:53 +0000

    Docker will absolutely work well for this — in fact, it solves all the i

--- Users/pola/Desktop/prediction-app/backend/server.js
+++ Users/pola/Desktop/prediction-app/backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const apiRoutes = require('./routes/api');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 // --- Middleware ---
 app.use(cors());
