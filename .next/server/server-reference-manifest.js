92ada5cf3f0de89fe3a9ebac14bd55df40e97781
Peter Olamilekan <polamedian@gmail.com>
2025-09-12 19:25:51 +0000

    all what you did was a mess

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"sCGCN6aJooY4IFQxlle6XbQHqWv3iyJw/HKPENnIA7M=\"\n}"
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -6,7 +5,7 @@ const mongoose = require('mongoose');
 const apiRoutes = require('./routes/api');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 // --- Middleware ---
 app.use(cors());
